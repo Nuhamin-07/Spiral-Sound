10f80f342b3de335ec505c1e09e8d62f2d67222d
Nuhamin <ngulilat@myfuse1.education>
2026-03-15 00:04:46 -0400

    modularized the server and populated genre dropdown

---
+++ controllers/productsControllers.js
@@ -0,0 +1,19 @@
+import { getDBConnection } from "../db/db.js";
+
+export async function getGenres(req, res) {
+  try {
+    const db = await getDBConnection();
+
+    const genreRows = await db.all("SELECT DISTINCT genre FROM products");
+    const genres = genreRows.map((row) => row.genre);
+    res.json(genres);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to fetch genres", details: err.message });
+  }
+}
+
+export async function getProducts() {
+  console.log("products");
+}
